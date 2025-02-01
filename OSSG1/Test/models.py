bb3090e54ab1c02bbd6b296a77fa6121da3f9f84
AL-D-Z <p2211323@mpu.edu.mo>
2025-02-01 17:06:17 +0800

    update

--- OSSG1/Test/models.py
+++ OSSG1/Test/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
 
 class UserInfo(models.Model):
     username = models.CharField('用户名', max_length=128)
