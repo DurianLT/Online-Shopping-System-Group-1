4c6c2160ba8e73de6fd0b416030ddbc5711815b4
DurianLT <liutianjunrong@outlook.com>
2025-02-01 19:55:37 +0800

    决定使用内嵌式的vue.js进行前端开发

--- frontend/src/about.js
+++
@@ -1,6 +0,0 @@
-// about.js
-import { createApp } from 'vue'
-import About from './pages/About.vue'
-
-const app = createApp(About)
-app.mount('#app')
--- frontend/src/home.js
+++
@@ -1,6 +0,0 @@
-// home.js
-import { createApp } from 'vue'
-import Home from './pages/Home.vue'
-
-const app = createApp(Home)
-app.mount('#app')
