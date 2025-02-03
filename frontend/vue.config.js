ae001e5f1368da9d391dfade13ae8991a4de0506
AL-D-Z <p2211323@mpu.edu.mo>
2025-02-03 09:43:10 +0800

    get new

--- frontend/vue.config.js
+++
@@ -1,6 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  outputDir: '../OSSG1/static/js',
-  publicPath: '/static/',
-})
